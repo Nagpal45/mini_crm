b8f10dabbfdd4d3294e3ad725ea62edab5fea63c
Nagpal45 <nagpal4503@gmail.com>
2024-06-11 16:34:28 +0530

    json

--- backend/index.js
+++ backend/index.js
@@ -21,6 +21,7 @@ require('./config/passport');
 const app = express();
 
 app.use(cors({ origin: 'https://mini-crm-1.onrender.com', credentials: true }));
+app.use(express.json());
 
 app.use(passport.initialize());
 app.use(session({
