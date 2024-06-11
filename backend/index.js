6b67fc6729c50a1fc567923e27b36e07ea36ad41
Nagpal45 <nagpal4503@gmail.com>
2024-06-11 14:47:14 +0530

    secure

--- backend/index.js
+++ backend/index.js
@@ -31,6 +31,7 @@ app.use(session({
     store: MongoStore.create({ mongoUrl: process.env.MONGO }),
     cookie: { 
         maxAge: 1000 * 60 * 60 * 24,
+        secure: true,
      }
 }));
 app.use(passport.session());
