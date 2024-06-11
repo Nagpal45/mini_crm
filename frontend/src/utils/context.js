83b889622afe3b9646228e092f4b3777947c72a0
Nagpal45 <nagpal4503@gmail.com>
2024-06-11 20:39:42 +0530

    fixed

--- frontend/src/utils/context.js
+++ frontend/src/utils/context.js
@@ -10,19 +10,6 @@ const AuthProvider = ({ children }) => {
         try {
             const response = await fetch('https://mini-crm-hnpl.onrender.com/auth/check-auth');
             let data = await response.json();
-            if (!data.authenticated) {
-                data = {
-                    authenticated: true,
-                    user: {
-                        _id: "6665cb374da141359ccd6733",
-                        googleId: "115969504386864776038",
-                        name: "Vaibhav",
-                        email: "vaibhavn0405@gmail.com",
-                        __v: 0
-                    }
-                };
-            }
-
             setIsAuthenticated(data.authenticated);
             setUser(data.user);
 
