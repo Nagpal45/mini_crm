f9151d3e5d5392b88e6e4eabf3bbbb72e7f74c56
Nagpal45 <nagpal4503@gmail.com>
2024-06-11 16:11:29 +0530

    urls

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
-export const login = (email, password) => axios.post('/auth/login', { email, password });
-export const getCustomerSize = (rules) => axios.post('/audience/check-size', { rules });
-export const createAudience = (name, rules) => axios.post('/audience', { name, rules });
-export const getAudiences = () => axios.get('/audience/all');
-export const getCampaigns = () => axios.get('/campaign');
-export const createCustomer = (customer) => axios.post('/data/customer', customer);
-export const createOrder = (order) => axios.post('/data/order', order);
-export const sendCampaign = (audienceId, message) => axios.post(`/campaign/${audienceId}`, { message });
-export const logout = () => axios.get('/auth/logout');
+export const login = (email, password) => axios.post('https://mini-crm-hnpl.onrender.com/auth/login', { email, password });
+export const getCustomerSize = (rules) => axios.post('https://mini-crm-hnpl.onrender.com/audience/check-size', { rules });
+export const createAudience = (name, rules) => axios.post('https://mini-crm-hnpl.onrender.com/audience', { name, rules });
+export const getAudiences = () => axios.get('https://mini-crm-hnpl.onrender.com/audience/all');
+export const getCampaigns = () => axios.get('https://mini-crm-hnpl.onrender.com/campaign');
+export const createCustomer = (customer) => axios.post('https://mini-crm-hnpl.onrender.com/data/customer', customer);
+export const createOrder = (order) => axios.post('https://mini-crm-hnpl.onrender.com/data/order', order);
+export const sendCampaign = (audienceId, message) => axios.post(`https://mini-crm-hnpl.onrender.com/campaign/${audienceId}`, { message });
+export const logout = () => axios.get('https://mini-crm-hnpl.onrender.com/auth/logout');
