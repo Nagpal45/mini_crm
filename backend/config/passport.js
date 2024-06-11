009b677072e638dcbe611583b9cd20a4ea7eb403
Nagpal45 <nagpal4503@gmail.com>
2024-06-11 13:05:52 +0530

    callback

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -5,7 +5,7 @@ const User = require('../models/user'); // Assume you have a User model
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: '/auth/google/callback'
+    callbackURL: 'https://mini-crm-hnpl.onrender.com/auth/google/callback'
 }, async (accessToken, refreshToken, profile, done) => {
     try {
         let user = await User.findOne({ googleId: profile.id });
